ce90c897751d92de648ba7315332dd12a74e9355
Bruno Rodrigues Ginciene <brunoginciene@hotmail.com>
2022-05-24 10:41:23 +0000

    iniciando lista

--- src/Teste.js
+++ src/Teste.js
@@ -1,21 +1,16 @@
-import React, {useState} from "react";
+import React from "react";
 
 export default function Teste(){
-    const [counter, setCounter] = useState(0)
-
-    function add(){
-        setCounter(counter + 1)
-    }
-
-    function clear(){
-        setCounter(0)
-    }
     
     return(
         <>
-            <h1>{counter}</h1>
-            <button onClick={add}>Add</button>
-            <button onClick={clear}>Clear</button>
+            <input placeholder="Write here"></input>
+            <button>Add</button>
+            <ul>
+                <li>
+                    Item da lista
+                </li>
+            </ul>
         </>
     )
 }
