b58394f238b0112e26367ab29b04c79ef119e521
Bruno Rodrigues Ginciene <brunoginciene@hotmail.com>
2022-05-24 17:28:26 +0000

    pegando valor

--- src/Teste.js
+++ src/Teste.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Teste(){
+    const [valor, setValor] = useState("")
+    //const [lista, setLista] = useState([])
     
+    function getValue(event){
+        setValor(event.target.value)
+    }
+
+    function addItem(){
+        alert(valor)
+        setValor("")
+    }
+
     return(
         <>
-            <input placeholder="Write here"></input>
-            <button>Add</button>
+            <input
+                onChange={getValue} 
+                placeholder="Write here"
+                value={valor}
+            >
+            </input>
+            <button onClick={addItem}>Add</button>
             <ul>
                 <li>
-                    Item da lista
+                    Item 1
                 </li>
             </ul>
         </>
